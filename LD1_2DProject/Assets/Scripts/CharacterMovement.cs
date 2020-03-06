ceb451e16ce5f55dd8e22c0ca4b2c3be2e57f603
RyanMillares <rmillares@chapman.edu>
2020-03-06 15:53:00 -0800

    it took 10 minutes to add, the commit will probs take an hour lol

---
+++ LD1_2DProject/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float speed = 6f;
+    public float sprintSpeed = 8f;
+    public float jumpSpeed = Mathf.Sqrt(50);
+    public float gravity = 10f;
+    private Vector3 moveDirection = Vector3.zero;
+    private float previousYVelocity = 0f;
+    public bool isJumping = false;
+    private float shortJumpMultiplier = 3f;
+    private float fallMultiplier = 4f;
+
+    [HideInInspector] public bool isFacingLeft = false;
+    public bool isMoving = false;
+    public bool isSprinting = false;
+    public double previousX;
+    public double currentX;
+    
+	/// SOUNDS
+	private AudioSource source;
+    public AudioClip jumpSound;
+    public AudioClip spawnSound;
+    
+	// Use this for initialization
+	void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(spawnSound);
+    }
+    
+	// Update is called once per frame
+	void Update()
+    {
+        previousX = System.Math.Round(this.gameObject.transform.position.x, 2);
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), previousYVelocity, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        
+        if (isSprinting)
+        {
+            moveDirection.x *= sprintSpeed;
+        }
+        if (!isSprinting)
+        {
+            moveDirection.x *= speed;
+        }
+
+        DBMovement();
+        
+		if (controller.isGrounded)
+        {
+            
+			isJumping = false;
+            moveDirection.y = 0;
+            
+			if (Input.GetButton("Jump") && !isJumping)
+            {
+                moveDirection.y = jumpSpeed;
+                isJumping = true;
+                source.PlayOneShot(jumpSound);
+            }
+        }
+        else
+        {
+            if (isJumping && Input.GetButton("Jump") && moveDirection.y > 0)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+            else if (moveDirection.y < 0)
+            {
+                moveDirection.y -= gravity * Time.deltaTime * (fallMultiplier);
+            }
+            else
+            {
+                moveDirection.y -= gravity * Time.deltaTime * (shortJumpMultiplier);
+            }
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+        currentX = System.Math.Round(this.gameObject.transform.position.x, 2);
+        //Check to see if we are currently moving... this is needed for animation purposes...
+        if (previousX != currentX)
+        {
+            isMoving = true;
+        }
+        else if (previousX == currentX)
+        {
+            isMoving = false;
+        }
+        
+        previousYVelocity = moveDirection.y;
+    }
+    
+	void DBMovement()
+    {
+        if (moveDirection.x > 0)
+        {
+            //Debug.Log ("I moved right!");
+            isFacingLeft = false;
+        }
+        else if (moveDirection.x < 0)
+        {
+            //Debug.Log ("I moved left!");
+            isFacingLeft = true;
+        }
+    }
+}
---
+++ LD1_2DProject/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public int distance;
+    public float range;
+    private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MoveItRight", 1f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveItRight()
+    {
+        Vector3 first = transform.position;
+        first.x += range;
+        transform.position = first;
+        counter++;
+        if(counter > distance)
+        {
+            CancelInvoke();
+            counter = 0;
+            InvokeRepeating("MoveItLeft", 0f, 0.1f);
+        }
+
+    }
+    public void MoveItLeft()
+    {
+        Vector3 first = transform.position;
+        first.x -= range;
+        transform.position = first;
+        counter++;
+        if (counter > distance)
+        {
+            CancelInvoke();
+            counter = 0;
+            InvokeRepeating("MoveItRight", 0f, 0.1f);
+        }
+    }
+}
